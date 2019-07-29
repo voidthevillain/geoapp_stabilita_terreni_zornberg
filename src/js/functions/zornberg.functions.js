35329d8033cd23a30a377f0de45e157eaafca820
Mihai Filip <voidthevillain@gmail.com>
2019-07-29 15:16:01 +0300

    change require into import

--- src/js/functions/zornberg.functions.js
+++ src/js/functions/zornberg.functions.js
@@ -1,9 +1,9 @@
-const { rad, sen, cos, tan} = require('../utils/ang.util')
+import { rad, sen, cos, tan } from '../utils/ang.util'
 
-exports.calcoloW = (y_terr, L, t_terr) => y_terr * L * t_terr
-exports.calcoloFSTot = (fs1, fs2, fs3, fs4) => fs1 * fs2 * fs3 * fs4
-exports.calcoloTamm = (t_nom, fs_tot) => t_nom / fs_tot
-exports.calcoloTp = (t_amm, Lt) => t_amm / Lt
-exports.calcoloN = (W, B) => W * cos(rad(B))
-exports.calcoloS = (W, B, tp, L) => W * sen(rad(B)) - tp * L
-exports.calcoloFSr = (c_terr, y_terr, t_terr, B, delta, tp) => ((c_terr / y_terr * t_terr * sen(rad(B))) + tan(rad(delta)) / tan(rad(B))) / (1 - (tp / (y_terr * t_terr * sen(rad(B))))) 
+export const calcoloW = (y_terr, L, t_terr) => y_terr * L * t_terr
+export const calcoloFSTot = (fs1, fs2, fs3, fs4) => fs1 * fs2 * fs3 * fs4
+export const calcoloTamm = (t_nom, fs_tot) => t_nom / fs_tot
+export const calcoloTp = (t_amm, Lt) => t_amm / Lt
+export const calcoloN = (W, B) => W * cos(rad(B))
+export const calcoloS = (W, B, tp, L) => W * sen(rad(B)) - tp * L
+export const calcoloFSr = (c_terr, y_terr, t_terr, B, delta, tp) => ((c_terr / y_terr * t_terr * sen(rad(B))) + tan(rad(delta)) / tan(rad(B))) / (1 - (tp / (y_terr * t_terr * sen(rad(B))))) 

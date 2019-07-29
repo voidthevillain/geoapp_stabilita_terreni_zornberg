7f421ef90865b40c1f8b88f1554fdec41f23f1ec
Mihai Filip <voidthevillain@gmail.com>
2019-07-29 15:27:56 +0300

    add webpack-merge and create separate config files

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
   // devtool: 'none', // no eval()
   entry: "./src/js/main.js",
-  output: {
-    filename: 'main.[contentHash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [new HtmlWebpackPlugin({
     template: './src/index.html'
   })],

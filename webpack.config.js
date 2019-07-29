e1ce5a43b66c64fbdc4b2fd00ca3f2c3dee137e3
Mihai Filip <voidthevillain@gmail.com>
2019-07-29 14:04:36 +0300

    add style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,18 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  devtool: 'none', // no eval()
+  // devtool: 'none', // no eval()
   entry: "./src/js/main.js",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
